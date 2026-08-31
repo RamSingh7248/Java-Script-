1ec7487d86e9ae9fc2c08f4c246d46d565d3cf82
RamSingh7248 <ramub9349@gmail.com>
2026-08-31 07:16:15 +0000

    Conversion

--- Basics.js
+++ Basics.js
@@ -1,1 +1,12 @@
-console.log("Ramu")
+const accountId = 10
+let email = "ramu@6535@gmail.com"
+var accountPassword = "12345"
+  accountCity = "Chhata"
+  email = "ramu@1334@gmail.com"
+  accountCity = "Mathura"
+
+  // accountId = 2
+  console.log(accountId);
+  console.log(email);
+  console.log(accountCity);
+  console.table([accountId, email, accountCity])
