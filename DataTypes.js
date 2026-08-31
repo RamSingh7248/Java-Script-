8d0c8ab7f945eb516278a775732116ecdc4686ff
RamSingh7248 <ramub9349@gmail.com>
2026-08-31 05:12:47 +0000

    Data

---
+++ DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3) // we are using node.js, not browser
+console.log(3 + 3) // code readability be high
+console.log("Ramu thakur")
+
+let name = "Ramu"
+let age = 18
+let isLogged = true
+
+// number => 2 to the power 53
+// bigint
+// String => ""
+// boolean => true or false
+// null => standalone value
+// undifined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof "Ramu");
+ 
